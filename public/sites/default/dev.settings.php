af16d184d98b9c28d6f11f8a07a5646d9736fb18
Campbell Vertesi <campbell@vertesi.com>
2014-11-26 18:04:25 +0000

    add dev settings

--- public/sites/default/dev.settings.php
+++ public/sites/default/dev.settings.php
@@ -1,6 +1,29 @@
 <?php
+// @file development settings for thecast.
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'thecast',
+      'username' => 'thecast',
+      'password' => 'thecast',
+      'host' => '127.0.0.1',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
-// @file: development settings for The Cast.
-
-$conf['stage_file_proxy_origin'] = 'http://thecastmusic.com';
 $conf['cache'] = '0';
+$conf['file_private_path'] = '../private';
+$conf['file_temporary_path'] = '/tmp';
+$conf['securepages_enable'] = FALSE;
+$conf['https'] = FALSE;
+$conf['stage_file_proxy_origin'] = 'http://thecastmusic.com';
+
+$conf['cache_backends'][] = 'sites/all/modules/memcache/memcache.inc';
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['memcache_key_prefix'] = 'thecast';
